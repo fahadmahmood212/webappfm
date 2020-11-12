184afdd83e1b9263dd7ef7ca8b55db04d4fb7a71
fahadmahmood212 <mahmood01fah@gmail.com>
2020-11-12 17:52:46 +0000

    created project with vue CLI

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# webappfm
-this is for my CST3145 coursework.
+# hello-world
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div v-for="lesson in lessons" :key="lesson.id"> 
+      <p>Subject: {{ lesson.subject}}</p>
+      <p>Location: {{ lesson.location}} </p>
+      <p>Price: {{ lesson.price}}</p>
+      <p>Spaces: {{ lesson.places}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import lessonData from "./lessons.json"; 
+export default {
+  name: 'App',
+  data() {
+    return{
+      lessons: lessonData,
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: 60,60,60;
+  margin-top: 60px;
+}
+</style>
