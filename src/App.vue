cdd0999091866c71b804b1fca18bdd9dce86d723
fahadmahmood212 <mahmood01fah@gmail.com>
2020-11-16 18:17:33 +0000

    added comments

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,145 @@
 <template>
-  <div id="app">
-    <div v-for="lesson in lessons" :key="lesson.id"> 
-      <p>Subject: {{ lesson.subject}}</p>
-      <p>Location: {{ lesson.location}} </p>
-      <p>Price: {{ lesson.price}}</p>
-      <p>Spaces: {{ lesson.places}}</p>
+<!-- this is for the html part of the code-->
+  <div id="app"> 
+    <!-- this is for the select button for different sorting categories-->
+    <select v-model="sortBy">
+      <option disabled value="">Sort by</option>
+      <option value="subject">Sort by Subject</option>
+      <option value="location">Sort by Location</option>
+      <option value="price">Sort by Price</option>
+      <option value="places">Sort by Places Available</option>
+    </select>
+    <!-- This is for the button for chosing the sort by order-->
+    <br>
+    <span>Sort by order: {{ inputSortingOrder }}</span>
+    <br>
+    <input type="radio" id="one" value="ASC" v-model="inputSortingOrder" >
+    <label for="one">Ascending</label>
+    <br>
+    <input type="radio" id="two" value="DESC" v-model="inputSortingOrder" >
+    <label for="two">Descending</label>
+    <br>
+    <!-- This is for displaying the lessons-->
+    <div v-for="lesson in sortedLessons" :key="lesson.id" v-bind:class="changeSortByOrder"> 
+      <!-- This is the list with lessons details-->
+      <ul>
+       <li>Subject: {{ lesson.subject}}</li>
+      <li>Location: {{ lesson.location}} </li>
+      <li>Price: {{ lesson.price}}</li>
+      <li>Spaces: {{ lesson.places}}</li> 
+      </ul>
+      <!-- This is for adding to chart-->
+      <button v-on:click="addtochart(this, lesson)" :disabled="isDisabled(lesson)"> addtochart </button>
     </div>
   </div>
 </template>
 
 <script>
-import lessonData from "./lessons.json"; 
+import lessonData from "./lessons.json"; // to import the lesson.json file
 export default {
   name: 'App',
   data() {
     return{
       lessons: lessonData,
+      sortBy: "",
+      inputSortingOrder: "ASC", //this is the order selected by the user
+      defaultSortingOrder: "ASC", //this is the current order being used 
     }
+  },
+  computed:{ 
+    //this is the function to sort the array of lessons
+    sortedLessons: function() {
+      // check if order needs updating
+      if(this.defaultSortingOrder != this.inputSortingOrder)
+      {
+        this.changeSortByOrder();
+      }
+      // to check what categories to sort by
+     switch(this.sortBy) {
+        case "subject":
+          return this.sortedBySubject();
+        case "location":
+          return this.sortedByLocation();
+        case "price":
+          return this.sortedByPrice();
+        case "places":
+          return this.sortedByPlacesAvailable();
+      }
+      return this.lessons;
+    }
+  },
+  methods:{ 
+    // update the lesson details after adding to chart
+    addtochart (button, lessontoadd) {
+      if(lessontoadd.places>0)
+      { 
+        let id = lessontoadd.id;   
+        this.lessons[id].places -= 1;
+      }
+      if(lessontoadd.places === 0)
+      {
+        button.style.visibility = "hidden";
+      }
+    },
+    // check if add to chart button should be disabled 
+     isDisabled(lesson) {
+      if (lesson.places === 0)
+        return true;
+      else
+        return false;
+    },
+    // change the current sorting order to the new one
+    changeSortByOrder: function () {
+      this.defaultSortingOrder = this.inputSortingOrder;
+    },
+    // sort lessons array by subject 
+    sortedBySubject: function () {
+      function compare(a, b) {
+        if (a.subject < b.subject)
+          return -1;
+        if (a.subject > b.subject)
+          return 1;
+        return 0;
+      }
+      let sortedArray = this.lessons;
+      return sortedArray.sort(compare);
+    },
+    // sort lessons array by subject 
+     sortedByLocation: function(){
+      function compare(a, b) {
+        if (a.location < b.location)
+          return -1;
+        if (a.location > b.location)
+          return 1;
+        return 0;
+      }
+      let sortedArray = this.lessons;
+      return sortedArray.sort(compare);
+    },
+    // sort lessons array by subject 
+     sortedByPrice: function(){
+      function compare(a, b) {
+        if (a.price< b.price)
+          return -1;
+        if (a.price> b.price)
+          return 1;
+        return 0;
+      }
+      let sortedArray = this.lessons;
+      return sortedArray.sort(compare);
+    }, 
+    // sort lessons array by subject 
+     sortedByPlacesAvailable: function(){
+      function compare(a, b) {
+        if (a.places < b.places)
+          return -1;
+        if (a.places > b.places)
+          return 1;
+        return 0;
+      }
+      let sortedArray = this.lessons;
+      return sortedArray.sort(compare);
+    },
   }
 }
 </script>
